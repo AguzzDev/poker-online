589f6c96091c465b7a3fb77b842341601aaa5e6c
AguzzDev <aribotta1901@gmail.com>
2024-07-12 16:08:07 -0300

    chore(cors): allow all connections

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { BadRequestException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule, { cors: { origin: '*' } });
 
   app.useGlobalPipes(
     new ValidationPipe({
