f877788fc38d6f981709237a0f046e993b260773
AguzzDev <aribotta1901@gmail.com>
2024-09-01 17:22:47 -0300

    chore: update metatags

--- client/components/Metatags.tsx
+++ client/components/Metatags.tsx
@@ -1,12 +1,14 @@
 export const Metatags = () => {
   return (
     <>
+      <link rel="canonical" href="https://poker.agustin-ribotta.xyz/" />
       <meta name="description" content="Poker Online by Agustín Ribotta" />
-
       <meta
-        property="og:url"
-        content="https://poker-app.agustin-ribotta.xyz/"
+        name="keywords"
+        content="poker online, play poker, poker tournaments, poker page, poker app, póker online, jugar póker, torneos de póker, juegos de cartas, póker Texas Hold'em, póker en línea, Agustín Ribotta, plataforma de póker"
       />
+
+      <meta property="og:url" content="https://poker.agustin-ribotta.xyz/" />
       <meta property="og:type" content="website" />
       <meta property="og:title" content="Poker Online" />
       <meta
@@ -14,12 +16,13 @@ export const Metatags = () => {
         content="Poker Online by Agustín Ribotta"
       />
       <meta property="og:image" content="/Logo2.png" />
+      <meta property="og:image:alt" content="Poker Online Logo" />
 
       <meta name="twitter:card" content="summary_large_image" />
       <meta property="twitter:domain" content="poker-app.agustin-ribotta.xyz" />
       <meta
         property="twitter:url"
-        content="https://poker-app.agustin-ribotta.xyz/"
+        content="https://poker.agustin-ribotta.xyz/"
       />
       <meta name="twitter:title" content="Poker Online" />
       <meta
@@ -27,6 +30,7 @@ export const Metatags = () => {
         content="Poker Online by Agustín Ribotta"
       />
       <meta name="twitter:image" content="/Logo2.png" />
+      <meta name="twitter:image:alt" content="Poker Online Logo" />
     </>
   );
 };
